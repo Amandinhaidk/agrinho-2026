0c1dfb82d9e1178e66b73f4f1dd73eb396c69048
Amandinhaidk <dias.moura@escola.pr.gov.br>
2026-05-25 16:02:12 -0300

    1

--- caracteriscas.js
+++ caracteriscas.js
@@ -6,19 +6,17 @@
 const btnPontinhos =document.getElementById('btnPontinhos');
 const abaCaracteristicas =document.getElementById('abaCaracteristicas')
 
-//FUNÇÃO CONTROLADORA (muda o estado de exibição do elemento)
+//2.FUNÇÃO CONTROLADORA (muda o estado de exibição do elemento)
 function alternarAbaCaracteristicas(){
     //Se a aba estiver escondida (none), ela aparece (block). Se ela estiver aparecendo, ela esconde.
     if (abaCaracteristicas.style.display === 'none') {
         abaCaracteristicas.style.display = 'block' ;
-        btnPontinhos.style.color = #344b75 // Muda os pontinhor para outra cor quando aberto
     } else {
         abaCaracteristicas.style.display = 'none';
-        btnPontinhos.style.color = #333; //Volta para a cor padrão quando fechado.
     }
 }
 
 //3. EVENT LISTENER (Dispara a função ao clicar)
 if (btnPontinhos && abaCaracteristicas) {
-    btnPontinhos.addEventListener('click', alternarAbaCaracteristicas);
+    btnPontinhos.addEventListener('click', alternarAbaCaracteristicas)
 }
